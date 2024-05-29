d5e79f8a79175b40d9a29a7d32ac2c5ca5c92c32
Vinícius Castilho <viniciuscastilho8@gmail.com>
2024-05-29 20:12:19 -0300

    cadastro FUNCIONANDO

--- php/conexao.php
+++ php/conexao.php
@@ -1,14 +1,8 @@
 <?php
-$host="localhost";
-$usuario= 'root'; 
-$senha= '';
-$database= 'login';
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "login";
 
-
-$mysqli = new mysqli($host, $usuario, $senha, $database);
-
-
-if($mysqli->connect_errno){
-    echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-
-}
+//Criar a conexao
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
---
+++ php/processa.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+include_once("conexao.php");
+
+// Sanitizar as entradas
+$cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_NUMBER_INT);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$senha = filter_input(INPUT_POST, 'senha', FILTER_DEFAULT);
+
+// Verificar se as variáveis foram definidas corretamente
+if (!$cpf || !$email || !$senha) {
+    die("Erro: Entrada inválida.");
+}
+
+// Corrigir o comando SQL
+$result_usuario = "INSERT INTO cadastro1 (email, cpf, senha, created_at) VALUES ('$email', '$cpf', '$senha', NOW())";
+
+// Executar a query e capturar erros
+if (mysqli_query($conn, $result_usuario)) {
+    // Verificar se o usuário foi inserido com sucesso
+    if(mysqli_insert_id($conn)){
+        $_SESSION['msg'] = "<p style='color:green;'>Usuário cadastrado com sucesso</p>";
+        header("Location: ../tela-processos.html");
+    } else {
+        $_SESSION['msg'] = "<p style='color:red;'>Usuário não foi cadastrado com sucesso</p>";
+        header("Location: index.php");
+    }
+} else {
+    // Capturar e exibir o erro de SQL
+    $error_message = mysqli_error($conn);
+    $_SESSION['msg'] = "<p style='color:red;'>Erro no cadastro: $error_message</p>";
+    header("Location: index.php");
+}
+?>
