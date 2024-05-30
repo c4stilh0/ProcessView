45f63a120fa2aa0fee728064a61617b7ba63067a
Vinícius Castilho <viniciuscastilho8@gmail.com>
2024-05-29 21:38:32 -0300

    login funcional

--- php/login.php
+++ php/login.php
@@ -7,21 +7,21 @@ if (isset($_POST['email']) && isset($_POST['senha'])) {
     } else if (strlen($_POST['senha']) == 0) {
         echo "Preencha sua senha";
     } else {
-        $email = $mysqli->real_escape_string($_POST['email']);
-        $senha = $mysqli->real_escape_string($_POST['senha']);
+        $email = $conn ->real_escape_string($_POST['email']);
+        $senha = $conn ->real_escape_string($_POST['senha']);
 
-        $sql_code = "SELECT * FROM usuario WHERE email='$email' AND senha='$senha'";
-        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+        $sql_code = "SELECT * FROM cadastro1 WHERE email='$email' AND senha='$senha'";
+        $sql_query = $conn ->query($sql_code) or die("Falha na execução do código SQL: " . $conn ->error);
         $quantidade = $sql_query->num_rows;
 
         if ($quantidade == 1) {
-            $usuario = $sql_query->fetch_assoc();
+            $cadastro1 = $sql_query->fetch_assoc();
 
             if (!isset($_SESSION)) {
                 session_start();
             }
-            $_SESSION['id'] = $usuario['id'];
-            $_SESSION['name'] = $usuario['nome'];
+            $_SESSION['id'] = $cadatro1['id'];
+            $_SESSION['verificacao'] = $cadatro1['verificacao'];
 
             header("Location: painel.php");
             exit();
