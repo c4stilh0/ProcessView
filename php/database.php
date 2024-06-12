6e1f257e4dbbbf1a634209072cde5c6bdc7284b2
Vinícius Castilho <122799944+c4stilh0@users.noreply.github.com>
2024-06-12 13:26:14 -0300

    Update database.php

--- php/database.php
+++ php/database.php
@@ -1,5 +1,5 @@
 <?php
-$host = '127.0.0.1'; // ou o endereço do seu servidor MySQL
+$host = 'dbloginserver.database.windows.net'; // ou o endereço do seu servidor MySQL
 $db = 'login';
 $user = 'root';
 $pass = '';
