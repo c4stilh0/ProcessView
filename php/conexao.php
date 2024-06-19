cd34e54b6ace1629d8167f91be7d08f0771249b9
Vinícius Castilho <122799944+c4stilh0@users.noreply.github.com>
2024-06-19 09:47:35 -0300

    Update conexao.php

--- php/conexao.php
+++ php/conexao.php
@@ -1,8 +1,8 @@
 <?php
-$servidor = "dbloginserver.database.windows.net";
-$usuario = "id22299978_pr0cessview2024";
-$senha = "EWb5$e.E76JzWQf";
-$dbname = "dblogin";
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "login";
 
 //Criar a conexao
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
